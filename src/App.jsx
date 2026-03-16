63386fe006b141290d50d8d2e993313229fcf119
Mehedi <mehedi.interface@gmail.com>
2026-03-16 13:54:01 +0800

    31-4 Explore JSX and Props basics

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,49 @@ function App() {
 
   return (
     <>
-    <h1>Vite + React</h1>
+    <h1>React Core Concepts</h1>
+    <Student></Student>
+    <Student></Student>
     <Person></Person>
-    <Person></Person>
-    <Person></Person>
-    <Sports></Sports>
+    <Developer name="Mozumder" tech="JS"></Developer>
+    <Developer name="Razon" tech="Python"></Developer>
     </>
+  )
+}
+
+function Developer(props) {
+  console.log(props)
+  return(
+    <div style={{
+      border: '2px solid green', 
+      borderRadius: '20px'
+    }}>
+      <h3>Developer: {props.name} </h3>
+      <p>Technology: {props.tech} </p>
+    </div>
   )
 }
 
 function Person () {
   const age = 17;
   const name = 'Jolil';  
+
+  const personStyle = {
+    color: 'red', 
+    textAlign: 'right'
+  }
+
   return (
-    <p>I am a person: {name} {age}</p>
+    <p id='' title='tooltip' style={personStyle}>I am a person: {name} {age}</p>
+  )
+}
+
+function Student() {
+  return (
+    <div className='student'>
+      <p>Name: </p>
+      <p>Dept: </p>
+    </div>
   )
 }
 
